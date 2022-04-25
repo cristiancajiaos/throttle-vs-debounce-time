6d5f700df29535219acf3887f6f5e434edf8c962
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-04-25 11:26:53 -0400

    Implementado uso de throttleTime

--- src/app/layout/throttle/throttle.component.ts
+++ src/app/layout/throttle/throttle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { throttleTime, pluck, map, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-throttle',
@@ -10,6 +11,34 @@ export class ThrottleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const cities = [
+      'rome',
+      'madrid',
+      'paris',
+      'brussels',
+      'eindhoven',
+      'berlin',
+      'copenhagen',
+      'stockholm',
+    ];
 
+    const filterCities = (value) => cities.filter((city) => city.indexOf(value) !== -1);
+
+    const input = document.querySelector('#inputThrottle');
+    const suggestions = document.querySelector('#suggestionsThrottle');
+
+    fromEvent(input, "keyup")
+      .pipe(
+        pluck("target", "value"),
+        throttleTime(500),
+        map(value => {
+          console.log("value is ", value);
+          return filterCities(value);
+        }),
+        map(filteredCities =>
+          filteredCities.map(city => `<li>${city}</li>`).join('')
+         )
+       )
+      .subscribe(html => (suggestions.innerHTML = html));
+    }
 }
